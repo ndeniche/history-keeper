093c0f354fe277ba9fa3fede6c440da905ce5810
Niche <ndeniche@gmail.com>
2020-01-27 01:19:10 -0600

    Created HistoryKeeper library

---
+++ src/history-keeper/HistoricEvent.ts
@@ -0,0 +1,11 @@
+class HistoricEvent {
+  private _beginDate: Date;
+  private _endDate: Date;
+  private _description: String;
+
+  constructor() {
+    
+  }
+}
+
+export default HistoricEvent;
---
+++ src/history-keeper/index.ts
@@ -0,0 +1,14 @@
+import HistoricEvent from './HistoricEvent';
+
+/**
+ * @author Rubén Reyes
+ */
+class HistoryKeeper {
+
+  private _events: Array<HistoricEvent>;
+
+  constructor() {
+    this._events = [];
+  }
+
+}
